e59adb83e2fe578e072f07320bf47ebab17198e3
Abdul-Kadar-web <emonkhan016844@gmail.com>
2021-02-27 15:38:25 +0600

    ultra nation practice

---
+++ src/components/Country/Country.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Country = (props) => {
+    const { name, population, region, flag } = props.country;
+    const flagStyle = { height: '50px' }
+   const handleAddCountry = props.handleAddCountry;
+    return (
+        <div style={{ border: '1px solid red', margin: '10px', padding: '10px' }}>
+            <h4>This is a {name}</h4>
+            <img style={flagStyle} src={flag} alt="" />
+            <p>population: {population}</p>
+            <p><small>Region: {region}</small></p>
+            <button onClick={() => handleAddCountry(props.country)}>Add Country</button>
+        </div>
+    );
+};
+
+export default Country;
